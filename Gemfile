ca36644ac732e344b09618e3c613d2a9473309e3
Hady Matar <hadyematar@gmail.com>
2023-04-12 14:06:32 -0600

    Removed Spotify references

--- Gemfile
+++ Gemfile
@@ -33,9 +33,7 @@ gem "bootsnap", require: false
 gem "faraday"
 gem "figaro"
 gem "jsonapi-serializer"
-gem 'rspotify'
 gem "ruby-openai"
-# gem "omniauth-spotify"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,7 +73,5 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data('<SPOTIFY_CLIENT>') { ENV['SPOTIFY_CLIENT'] }
-  config.filter_sensitive_data('<SPOTIFY_SECRET>') { ENV['SPOTIFY_SECRET'] }
   config.filter_sensitive_data('<Open_AI>') { ENV['Open_AI'] }
 end
