ab869e3757e624c783b89fb569f63971caad0c46
Hady Matar <hadyematar@gmail.com>
2023-04-12 23:23:57 -0600

    Playlist created but testing not completed

--- spec/requests/playlist_request_spec.rb
+++ spec/requests/playlist_request_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe "playlist", type: :request do
       end
 
     end
-    it "returns to you 10 songs in a playlist with a different character and a different genre", :vcr do 
+    it "each time the user inputs and makes a playlist, it creates a playlist that persists in the database", :vcr do 
       Theme.create(name: "lawyer")
       create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 0)
       create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 1)
@@ -57,23 +57,9 @@ RSpec.describe "playlist", type: :request do
       Character.create!(name: "Michael Cohen", theme_id: Theme.first.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.34, lawful_max: 0.66, url: "cohenurl.com", alignment: 8)
       bundy = Character.create!(name: "Ted Bundy", theme_id: Theme.first.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.00, lawful_max: 0.33, url: "url.com", alignment: 9)
       
+      expect(Playlist.all.count).to eq(0)
       get "/chordsapi/v1/themes/#{Theme.first.id}/characters/#{bundy.id}/playlists?genre=reggaton"
-
-      parsed_playlist = JSON.parse(response.body, symbolize_names: true)
-      expect(parsed_playlist).to be_a(Hash)
-      expect(parsed_playlist[:links]).to be_a(Hash)
-      expect(parsed_playlist[:links][:image]).to be_a(String)
-      expect(parsed_playlist[:data]).to be_a(Hash)
-      expect(parsed_playlist[:data][:type]).to eq("playlist")
-      expect(parsed_playlist[:data][:genre]).to be_a(String)
-      expect(parsed_playlist[:data][:attributes]).to be_a(Hash)
-      expect(parsed_playlist[:data][:attributes][:character_name]).to be_a(String)
-      expect(parsed_playlist[:data][:attributes][:quiz_theme]).to be_a(String)
-      expect(parsed_playlist[:data][:attributes][:character_alignment]).to be_a(String)
-      expect(parsed_playlist[:data][:attributes][:song_titles]).to be_a(Array)
-      parsed_playlist[:data][:attributes][:song_titles].each do |song|
-        expect(song).to be_a(String)
-      end
+      expect(Playlist.all.count).to eq(1)
 
     end
   end 
