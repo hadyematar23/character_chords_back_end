0ccca285709ceb5701949223646a78aafd21bb02
Harrison Ryan <hwryan12@gmail.com>
2023-04-16 19:04:06 -0500

    refactor: Remove limit on BE

--- db/migrate/20230416180841_add_limit_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class AddLimitToGenres < ActiveRecord::Migration[7.0]
-  def change
-    change_column :playlists, :genre, :string, limit: 36
-  end
-end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
+
   describe 'validations' do
     it { should validate_length_of(:genre).is_at_most(36) }
   end
