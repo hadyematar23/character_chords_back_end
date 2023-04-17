963e72ebaaebbf041f97fe1835813bdc5050d3eb
Harrison Ryan <hwryan12@gmail.com>
2023-04-16 19:30:19 -0500

    refactor: Add proper validations to playlist

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,4 +1,20 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Playlist, type: :model do
-# end
+RSpec.describe Playlist, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:genre) }
+    it { should validate_presence_of(:character) }
+    it { should validate_presence_of(:alignment) }
+    it { should validate_presence_of(:songs) }
+    
+    it 'validates that songs is an array' do
+      playlist = Playlist.new(
+        genre: 'Rock',
+        character: 'John',
+        alignment: 'Lawful Good',
+        songs: ['array', 'of', 'songs']
+      )
+      expect(playlist).to be_valid
+    end
+  end
+end
