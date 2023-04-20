597baa8dc68dba2d5c5bef9c51dc4405a215dafa
Harrison Ryan <hwryan12@gmail.com>
2023-04-20 11:03:15 -0500

    tests: Add edge case and sad path to playlist_facade_spec

--- app/facades/playlist_facade.rb
+++ app/facades/playlist_facade.rb
@@ -1,6 +1,7 @@
 class PlaylistFacade
 
   def generate_playlist(info)
-     playlist = PlaylistService.new.generate_playlists(info)
+    raise ArgumentError, "Genre cannot be empty or nil" if info[:genre].nil? || info[:genre].empty?
+    playlist = PlaylistService.new.generate_playlists(info)
   end
 end
