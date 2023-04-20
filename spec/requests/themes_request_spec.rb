b26e498e6d6ae8168f49b5d95cb64aded9df91ed
Harrison Ryan <hwryan12@gmail.com>
2023-04-20 13:07:55 -0500

    test: Add edge case/sad path to themes

--- spec/requests/themes_request_spec.rb
+++ spec/requests/themes_request_spec.rb
@@ -27,5 +27,27 @@ RSpec.describe "themes", type: :request do
         expect(theme[:attributes][:image]).to be_a(String)
       end
     end
+    
+    it "returns an empty array when there are no themes" do
+      get "/chordsapi/v1/themes"
+    
+      parsed_items = JSON.parse(response.body, symbolize_names: true)
+    
+      expect(response).to have_http_status(200)
+      expect(parsed_items).to have_key(:data)
+      expect(parsed_items[:data]).to be_an(Array)
+      expect(parsed_items[:data].count).to eq(0)
+    end
+
+    it "returns an empty array if there are no themes" do
+      get "/chordsapi/v1/themes"
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(200)
+      expect(parsed_response).to have_key(:data)
+      expect(parsed_response[:data]).to be_a(Array)
+      expect(parsed_response[:data]).to be_empty
+    end
   end
 end
