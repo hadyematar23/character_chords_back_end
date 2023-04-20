98d569c7451e440b14f9aa5e46dcbde0c2efb0d9
Harrison Ryan <hwryan12@gmail.com>
2023-04-20 12:50:01 -0500

    test: Add sad path to playlist endpoint

--- app/controllers/chordsapi/v1/playlists_controller.rb
+++ app/controllers/chordsapi/v1/playlists_controller.rb
@@ -1,16 +1,19 @@
 class Chordsapi::V1::PlaylistsController < ApplicationController
 
   def create
-    playlist = PlaylistFacade.new.generate_playlist(params)
-    character = Character.find(params[:character_id])
-    theme = character.theme
-    
-    created_playlist = Playlist.create!(genre: params[:genre], character: character.name, alignment: character.alignment, songs: [playlist[:choices].first[:message][:content]])    
-    
-    serialized_playlist= PlaylistSerializer.new.generate_playlist(created_playlist, character, theme)
+    begin
+      playlist = PlaylistFacade.new.generate_playlist(params)
+      character = Character.find(params[:character_id])
+      theme = character.theme
+      
+      created_playlist = Playlist.create!(genre: params[:genre], character: character.name, alignment: character.alignment, songs: [playlist[:choices].first[:message][:content]])    
+      
+      serialized_playlist= PlaylistSerializer.new.generate_playlist(created_playlist, character, theme)
 
-    render json: serialized_playlist
-  
+      render json: serialized_playlist
+    rescue ArgumentError => e
+      render json: { error: e.message }, status: :bad_request
+    end
   end
   
   def show
--- spec/requests/playlist_request_spec.rb
+++ spec/requests/playlist_request_spec.rb
@@ -3,35 +3,34 @@ require 'rails_helper'
 RSpec.describe "playlist", type: :request do
   describe "POST /api/v1/themes/:theme_id/characters/:character_id/playlist?genre=hip-hop" do
     before(:each) do 
-      Theme.create(name: "lawyer")
+      @theme = Theme.create!(name: "lawyer", s3_key: "test", image_link: "test_image") 
       create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 0)
       create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 1)
 
-      Character.create!(name: "Thurgood Marshall", theme_id: Theme.first.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.67, lawful_max: 1.00, url: "url.com", alignment: 1)
-      Character.create!(name: "Amal Clooney", theme_id: Theme.first.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.34, lawful_max: 0.66, url: "url.com", alignment: 2)
-      Character.create!(name: "Clarence Darrow", theme_id: Theme.first.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.00, lawful_max: 0.33, url: "url.com", alignment: 3)
-
-      Character.create!(name: "Jack McCoy", theme_id: Theme.first.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.67, lawful_max: 1.00, url: "url.com", alignment: 4)
-      Character.create!(name: "Robert Shapiro", theme_id: Theme.first.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.34, lawful_max: 0.66, url: "url.com", alignment: 5)
-      @jimmy = Character.create!(name: "Saul Goodman", theme_id: Theme.first.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.00, lawful_max: 0.33, url: "url.com", alignment: 6)
-
-      Character.create!(name: "Roy Cohn", theme_id: Theme.first.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.67, lawful_max: 1.00, url: "url.com", alignment: 7)
-      Character.create!(name: "Michael Cohen", theme_id: Theme.first.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.34, lawful_max: 0.66, url: "cohenurl.com", alignment: 8)
-      Character.create!(name: "Ted Bundy", theme_id: Theme.first.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.00, lawful_max: 0.33, url: "url.com", alignment: 9)
+      Character.create!(name: "Thurgood Marshall", theme_id: @theme.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.67, lawful_max: 1.00, s3key: 'LawyerCharacters/ThurgoodMarshall.jpeg', alignment: 1, description: "This is a test")
+      Character.create!(name: "Amal Clooney", theme_id: @theme.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.34, lawful_max: 0.66, s3key: 'LawyerCharacters/AmalClooney.jpeg', alignment: 2, description: "This is a test")
+      Character.create!(name: "Clarence Darrow", theme_id: @theme.id, good_min: 0.67, good_max: 1.00, lawful_min: 0.00, lawful_max: 0.33, s3key: 'LawyerCharacters/ClarenceDarrow.jpeg', alignment: 3, description: "This is a test" )
+      
+      Character.create!(name: "Jack McCoy", theme_id: @theme.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.67, lawful_max: 1.00, s3key: 'LawyerCharacters/JackMccoy.jpeg', alignment: 4, description: "This is a test" )
+      Character.create!(name: "Robert Shapiro", theme_id: @theme.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.34, lawful_max: 0.66, s3key: 'LawyerCharacters/RobertShapiro.jpeg', alignment: 5, description: "This is a test")
+      @jimmy = Character.create!(name: "Saul Goodman AKA Slippin' Jimmy", theme_id: @theme.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.00, lawful_max: 0.33, s3key: 'LawyerCharacters/SaulGoodman.webp', alignment: 6, description: "This is a test")
+      
+      Character.create!(name: "Roy Cohn", theme_id: @theme.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.67, lawful_max: 1.00, s3key: 'LawyerCharacters/RoyCohn.webp', alignment: 7, description: "This is a test")
+      Character.create!(name: "Michael Cohen", theme_id: @theme.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.34, lawful_max: 0.66, s3key: 'LawyerCharacters/MichaelCohen.jpeg', alignment: 8, description: "This is a test")
+      Character.create!(name: "Rudy Guiliani", theme_id: @theme.id, good_min: 0.00, good_max: 0.33, lawful_min: 0.00, lawful_max: 0.33, s3key: 'LawyerCharacters/RudyGuiliani.webp', alignment: 9, description: "This is a test")
     end
 
     it "creates a playlist, saves it to the database, and then redirects to the show page, returns to you a serialized JSON playlist with 10 songs in it", :vcr do 
 
       expect(Playlist.count).to eq(0)
 
-      post "/chordsapi/v1/themes/#{Theme.first.id}/characters/#{@jimmy.id}/playlists?genre=hip-hop"
-
+      post "/chordsapi/v1/themes/#{@theme.id}/characters/#{@jimmy.id}/playlists?genre=hip-hop"
       parsed_playlist = JSON.parse(response.body, symbolize_names: true)
+
       expect(Playlist.count).to eq(1)
       expect(response.status).to eq(200)
       expect(parsed_playlist).to be_a(Hash)
       expect(parsed_playlist[:links]).to be_a(Hash)
-      expect(parsed_playlist[:links][:image]).to be_a(String)
       expect(parsed_playlist[:data]).to be_a(Hash)
       expect(parsed_playlist[:data][:type]).to eq("playlist")
       expect(parsed_playlist[:data][:genre]).to be_a(String)
@@ -70,5 +69,24 @@ RSpec.describe "playlist", type: :request do
         expect(song).to be_a(String)
       end
     end
-  end 
+  end
+  describe "sad path" do
+    before(:each) do 
+      @theme = Theme.create!(name: "lawyer", s3_key: "test", image_link: "test_image") 
+      create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 0)
+      create_list(:question, 6, theme: Theme.all.first, good_evil_chaotic_lawful: 1) 
+      Character.create!(name: "Saul Goodman AKA Slippin' Jimmy", theme_id: @theme.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.00, lawful_max: 0.33, s3key: 'LawyerCharacters/SaulGoodman.webp', alignment: 6, description: "This is a test")
+    end   
+
+    it "returns a 400 status code if an invalid genre value is provided", :vcr do 
+      @theme = Theme.create!(name: "lawyer", s3_key: "test", image_link: "test_image") 
+
+      Character.create!(name: "Saul Goodman AKA Slippin' Jimmy", theme_id: @theme.id, good_min: 0.34, good_max: 0.66, lawful_min: 0.00, lawful_max: 0.33, s3key: 'LawyerCharacters/SaulGoodman.webp', alignment: 6, description: "This is a test")
+      
+      expect(Playlist.count).to eq(0)
+
+      post "/chordsapi/v1/themes/#{Theme.first.id}/characters/#{Character.first.id}/playlists?genre="
+      expect(response.status).to eq(400)
+    end
+  end
 end 
